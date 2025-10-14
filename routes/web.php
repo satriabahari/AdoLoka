95f4777562c2a2a9fe54bfc57cebbbbb664452c5
satria bahari <satriaabaharii@gmail.com>
2025-10-14 15:19:57 +0700

    feat: add some routes

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,31 @@
 <?php
 
+use App\Http\Controllers\EventController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
+use Laravel\Socialite\Facades\Socialite;
 
 Route::view('/', 'welcome');
+
+Route::view('home', 'home')
+    ->middleware(['auth', 'verified'])
+    ->name('home');
+
+Route::get('events', [EventController::class, 'index'])
+    ->middleware(['auth', 'verified'])
+    ->name('events');
+
+Route::get('events/{event}', [EventController::class, 'show'])
+    // Route::get('events/{slug}', [EventController::class, 'show'])
+    ->name('events.show');
+
+Route::get('products', [ProductController::class, 'index'])
+    ->middleware(['auth', 'verified'])
+    ->name('products');
+
+Route::get('products/{product}', [ProductController::class, 'show'])
+    // ->middleware(['auth', 'verified'])
+    ->name('products.show');
 
 Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
@@ -12,4 +35,14 @@ Route::view('profile', 'profile')
     ->middleware(['auth'])
     ->name('profile');
 
-require __DIR__.'/auth.php';
+Route::get('/auth/redirect', function () {
+    return Socialite::driver('google')->redirect();
+});
+
+Route::get('/auth/callback', function () {
+    $user = Socialite::driver('google')->user();
+
+    // $user->token
+});
+
+require __DIR__ . '/auth.php';
